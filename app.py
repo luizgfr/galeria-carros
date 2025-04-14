5802ce937c63036556e9cbf9ff670ca13a6df65d
Luiz Guilherme Franca <luiz.gfr2002@gmail.com>
2025-04-14 19:18:14 -0300

    Adicionando botão de editar e excluir

--- app.py
+++ app.py
@@ -33,5 +33,34 @@ def upload():
             return redirect(url_for('index'))
     return render_template('upload.html')
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT caminho FROM imagens WHERE id = %s", (id,))
+    imagem = cur.fetchone()
+
+    if imagem:
+        caminho_arquivo = os.path.join(app.config['UPLOAD_FOLDER'], imagem[0])
+        if os.path.exists(caminho_arquivo):
+            os.remove(caminho_arquivo)
+
+        cur.execute("DELETE FROM imagens WHERE id = %s", (id,))
+        mysql.connection.commit()
+    
+    return redirect(url_for('index'))
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    cur = mysql.connection.cursor()
+    if request.method == 'POST':
+        novo_nome = request.form['nome']
+        cur.execute("UPDATE imagens SET nome = %s WHERE id = %s", (novo_nome, id))
+        mysql.connection.commit()
+        return redirect(url_for('index'))
+
+    cur.execute("SELECT * FROM imagens WHERE id = %s", (id,))
+    imagem = cur.fetchone()
+    return render_template('edit.html', imagem=imagem)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000, debug=True)
